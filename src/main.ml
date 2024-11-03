1ebe30a9fb1f47ca2f9c831856164ee254253aaa
MgShepherd <michaelshep1504@gmail.com>
2024-11-03 09:53:50 +0000

    Added first example language file and setup reading from file

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let file_name = "examples/basic.mgs"
+
+let () =
+  let file = open_in file_name in
+  try
+    let line = input_line file in
+    prerr_endline line;
+    close_in file
+  with e ->
+    close_in_noerr file;
+    raise e

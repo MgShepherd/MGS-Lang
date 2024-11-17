1bc1962c078fcc170523a15c95a82cba4b7ea405
michaelshepherd <michaelshepherd@debian.michael.development.net>
2024-11-17 22:08:07 +0000

    Setup basic file writing functionality

---
+++ src/arm_gen.ml
@@ -0,0 +1,1 @@
+let generate_assembly _program = "Hello World! This should be assembly"
--- src/main.ml
+++ src/main.ml
@@ -1,17 +1,21 @@
-let file_name = "examples/basic.mgs"
+let file_name = "examples/basic"
 
 let process_file file =
   try
     let tokens = Lexer.process_file file in
     Token.display_tokens tokens;
-    let tree = Parser.create_tree tokens in
-    print_endline "\nParse Tree output:";
-    Printer.display_tree tree;
-    close_in file
-  with Failure e -> Printf.printf "Unexpected Exception Occurred\n%s" e
+    close_in file;
+    Parser.create_tree tokens
+  with Failure e ->
+    raise (Failure (Printf.sprintf "Unexpected Exception Occurred\n%s" e))
+
+let write_string contents =
+  let oc = open_out (file_name ^ ".s") in
+  Printf.fprintf oc "%s\n" contents;
+  close_out oc
 
 let () =
-  let file = try Some (open_in file_name) with _ -> None in
+  let file = try Some (open_in (file_name ^ ".mgs")) with _ -> None in
   match file with
-  | Some x -> process_file x
+  | Some x -> write_string (Arm_gen.generate_assembly (process_file x))
   | None -> Printf.printf "Unable to open file: %s\n" file_name

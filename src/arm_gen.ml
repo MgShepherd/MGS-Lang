16c30d25179f2adbddc090ff41c706abf1ad00c4
michaelshepherd <michaelshepherd@debian.michael.development.net>
2024-11-18 20:28:24 +0000

    Started generation of assembly code from program

--- src/arm_gen.ml
+++ src/arm_gen.ml
@@ -1,1 +1,13 @@
-let generate_assembly _program = "Hello World! This should be assembly"
+let create_start_function = ".global _start\n_start:\n"
+let process_expression _expr = "Parsing a statement\n"
+
+let process_statement = function
+  | Parser.AssignmentStatement (_, _, _, expr) -> process_expression expr
+
+let rec process_statements acc = function
+  | [] -> acc
+  | x :: xs -> process_statements (acc ^ process_statement x) xs
+
+let generate_assembly = function
+  | Parser.Program statements ->
+      create_start_function ^ process_statements "" statements

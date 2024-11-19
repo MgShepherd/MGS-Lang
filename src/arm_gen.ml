5c3f3748398d7f77ecc7f4aa865810a96ef3e2cf
michaelshepherd <michaelshepherd@debian.michael.development.net>
2024-11-19 20:10:20 +0000

    Enabled processing of add expression to create basic assembly program

--- src/arm_gen.ml
+++ src/arm_gen.ml
@@ -1,5 +1,11 @@
 let create_start_function = ".global _start\n_start:\n"
-let process_expression _expr = "Parsing a statement\n"
+
+let rec process_expression = function
+  | Parser.ExprToken (T_VALUE x) -> Printf.sprintf "#%s" x
+  | Parser.ExprArithmetic (T_ARITHMETIC "+", left, right) ->
+      Printf.sprintf "\tMOV X1, %s\n\tADD X0, X1, %s\n"
+        (process_expression left) (process_expression right)
+  | _ -> ""
 
 let process_statement = function
   | Parser.AssignmentStatement (_, _, _, expr) -> process_expression expr
@@ -8,6 +14,10 @@ let rec process_statements acc = function
   | [] -> acc
   | x :: xs -> process_statements (acc ^ process_statement x) xs
 
+let create_exit_function = "\tMOV X8, #93\n\tSVC 0\n"
+
 let generate_assembly = function
   | Parser.Program statements ->
-      create_start_function ^ process_statements "" statements
+      create_start_function
+      ^ process_statements "" statements
+      ^ create_exit_function

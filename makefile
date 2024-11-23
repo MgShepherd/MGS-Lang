583b74d1fef8729e5cbf944539e12ac73049151f
MgShepherd <michaelshepherd@debian.michael.development.net>
2024-11-23 16:48:04 +0000

    Moved generated files to build folder and setup makefile

---
+++ makefile
@@ -0,0 +1,14 @@
+S_FILES = build/basic.s
+OBJ_FILES = build/basic.o
+OUT_DIR = build
+
+%.o : %.s | $(OUT_DIR)
+	as -g $< -o $@
+
+main: $(OBJ_FILES) 
+	ld -o $(OUT_DIR)/main $(OBJ_FILES)
+
+all: main
+
+$(OUT_DIR):
+	mkdir -p $(OUT_DIR)
--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,6 @@
-let file_name = "examples/basic"
+let input_file_name = "examples/basic.mgs"
+let output_dir = "build"
+let output_file_name = output_dir ^ "/basic.s"
 
 let process_file file =
   try
@@ -12,12 +14,13 @@ let process_file file =
     raise (Failure (Printf.sprintf "Unexpected Exception Occurred\n%s" e))
 
 let write_string contents =
-  let oc = open_out (file_name ^ ".s") in
+  if not (Sys.file_exists output_dir) then Sys.mkdir output_dir 0o755;
+  let oc = open_out output_file_name in
   Printf.fprintf oc "%s\n" contents;
   close_out oc
 
 let () =
-  let file = try Some (open_in (file_name ^ ".mgs")) with _ -> None in
+  let file = try Some (open_in input_file_name) with _ -> None in
   match file with
   | Some x -> write_string (Arm_gen.generate_assembly (process_file x))
-  | None -> Printf.printf "Unable to open file: %s\n" file_name
+  | None -> Printf.printf "Unable to open file: %s\n" input_file_name

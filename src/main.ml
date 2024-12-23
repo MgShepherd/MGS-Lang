64182232bc5478e776d6625fbc5d3612e8c00772
MgShepherd <michaelshep1504@gmail.com>
2024-12-23 16:58:54 +0000

    Setup logger to start handling errors better - updated main.ml logging to use this

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,5 @@
+open Logger
+
 let output_dir = "build/"
 let assembly_extension = ".s"
 let object_extension = ".o"
@@ -9,24 +11,23 @@ let arg_usage_msg = Printf.sprintf "Usage: ./%smain -f <input_file>" output_dir
 let rec get_last_element = function
   | x :: [] -> x
   | _ :: xs -> get_last_element xs
-  | [] -> raise (Failure "Attempted to get last element of empty list")
+  | [] -> fatal_err "Provided file path is empty"
 
 let get_file_name path =
   let name_with_ext = get_last_element (String.split_on_char '/' path) in
   match String.split_on_char '.' name_with_ext with
   | x :: _ -> x
-  | [] -> raise (Failure "Unable to remove file extension")
+  | [] ->
+      fatal_err
+        "Please ensure that a valid file with the .mgs extension is provided"
 
 let process_file file =
-  try
-    let tokens = Lexer.process_file file in
-    Token.display_tokens tokens;
-    close_in file;
-    let tree = Parser.create_tree tokens in
-    Printer.display_tree tree;
-    tree
-  with Failure e ->
-    raise (Failure (Printf.sprintf "Unexpected Exception Occurred\n%s" e))
+  let tokens = Lexer.process_file file in
+  Token.display_tokens tokens;
+  close_in file;
+  let tree = Parser.create_tree tokens in
+  Printer.display_tree tree;
+  tree
 
 let write_string file_name contents =
   if not (Sys.file_exists output_dir) then Sys.mkdir output_dir 0o755;
@@ -34,22 +35,23 @@ let write_string file_name contents =
   Printf.fprintf oc "%s\n" contents;
   close_out oc
 
+let run_command command =
+  match Sys.command command with
+  | 0 -> ()
+  | _ ->
+      fatal_err
+        (Printf.sprintf
+           "Unable to run following command when generating executable:\n%s"
+           command)
+
 let produce_executable file_name =
-  ignore
-    (Sys.command
-       (Printf.sprintf "as -o %s %s"
-          (output_dir ^ file_name ^ object_extension)
-          (output_dir ^ file_name ^ assembly_extension)));
-  ignore
-    (Sys.command
-       (Printf.sprintf "ld -o %s %s" (output_dir ^ file_name)
-          (output_dir ^ file_name ^ object_extension)));
-  ignore
-    (Sys.command
-       (Printf.sprintf "rm %s" (output_dir ^ file_name ^ object_extension)));
-  ignore
-    (Sys.command
-       (Printf.sprintf "rm %s" (output_dir ^ file_name ^ assembly_extension)))
+  let obj_file = output_dir ^ file_name ^ object_extension in
+  let as_file = output_dir ^ file_name ^ assembly_extension in
+  let exe_file = output_dir ^ file_name in
+  run_command (Printf.sprintf "as -o %s %s" obj_file as_file);
+  run_command (Printf.sprintf "ld -o %s %s" exe_file obj_file);
+  run_command (Printf.sprintf "rm %s" obj_file);
+  run_command (Printf.sprintf "rm %s" as_file)
 
 let () =
   Arg.parse arglist anon_arg_func arg_usage_msg;
@@ -59,4 +61,6 @@ let () =
   | Some x ->
       write_string file_name (Arm_gen.generate_assembly (process_file x));
       produce_executable file_name
-  | None -> Printf.printf "Unable to open file: %s\n" !input_file_path
+  | None ->
+      fatal_err
+        (Printf.sprintf "Unable to open file with name: \"%s\"" file_name)

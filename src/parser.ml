66a35aa9fe1d84f9c4e4fe6bb3c2f5406a0b5438
MgShepherd <michaelshep1504@gmail.com>
2024-12-27 20:53:00 +0000

    Made parser use logger functionality for error handling

--- src/parser.ml
+++ src/parser.ml
@@ -1,4 +1,5 @@
 open Token
+open Logger
 
 type expr =
   | ExprArithmetic of token * expr * expr
@@ -12,65 +13,56 @@ type statement =
 
 type program = Program of statement list
 
-let unprocessable_token_error token =
-  let error_message =
-    Printf.sprintf "Parser Error: Unprocessable Token: %s\n"
-      (get_token_string token)
-  in
-  raise (Failure error_message)
+let unexpected_token x =
+  let error_message = Printf.sprintf "Unexpected Token: \"%s\"" x.t_str in
+  fatal_err_with_line error_message x.line_num
 
 let rec parse_open_paren nesting xs =
   let result, remaining = parse_expression None (T_OPEN_PAREN :: nesting) xs in
   parse_expression (Some result) nesting remaining
 
-and parse_close_paren prev nesting xs =
+and parse_close_paren x prev nesting xs =
   match prev with
-  | None -> raise (Failure "Empty Paratheses\n")
-  | Some x -> (
+  | None -> fatal_err_with_line "Empty Paratheses" x.line_num
+  | Some y -> (
       match nesting with
-      | T_OPEN_PAREN :: _ -> (x, xs)
-      | _ -> raise (Failure "Unbalanced Parentheses\n"))
+      | T_OPEN_PAREN :: _ -> (y, xs)
+      | _ -> unexpected_token x)
 
 and parse_value x prev nesting xs =
   match prev with
-  | Some _ -> raise (Failure "Invalid value location\n")
+  | Some _ -> unexpected_token x
   | None -> parse_expression (Some (ExprToken x)) nesting xs
 
 and parse_arithmetic x prev nesting xs =
   match prev with
-  | None -> raise (Failure "Invalid arithmetic operator\n")
+  | None -> unexpected_token x
   | Some left ->
       let right, remaining = parse_expression None nesting xs in
       (ExprArithmetic (x, left, right), remaining)
 
 and parse_comparision x prev nesting xs =
   match prev with
-  | None -> raise (Failure "Invalid comparision operator\n")
+  | None -> unexpected_token x
   | Some left ->
       let right, remaining = parse_expression None nesting xs in
       (ExprComparison (x, left, right), remaining)
 
 and handle_empty_tokens prev =
-  match prev with
-  | Some x -> (x, [])
-  | _ -> raise (Failure "Unproccessed Elements\n")
+  match prev with Some x -> (x, []) | _ -> fatal_err "Unproccessed Elements"
 
 and parse_expression prev nesting = function
   | x :: xs -> (
       match x.t_type with
       | T_OPEN_PAREN -> parse_open_paren nesting xs
-      | T_CLOSE_PAREN -> parse_close_paren prev nesting xs
+      | T_CLOSE_PAREN -> parse_close_paren x prev nesting xs
       | T_STRING -> parse_value x prev nesting xs
       | T_NUMBER -> parse_value x prev nesting xs
       | T_VARIABLE -> parse_value x prev nesting xs
       | T_ARITHMETIC -> parse_arithmetic x prev nesting xs
       | T_COMPARISON -> parse_comparision x prev nesting xs
       | T_ELSE -> (ExprToken x, [])
-      | _ ->
-          let error_message =
-            Printf.sprintf "Unrecognised Token %s\n" (get_token_string x)
-          in
-          raise (Failure error_message))
+      | _ -> unexpected_token x)
   | [] -> handle_empty_tokens prev
 
 let is_assignment_statement x y z =
@@ -83,11 +75,11 @@ let rec parse_statement = function
   | x :: y :: z :: xs when is_assignment_statement x y z ->
       let expression, remaining = parse_expression None [] xs in
       if List.length remaining > 0 then
-        raise (Failure "Unproccessed tokens in statement\n")
+        fatal_err_with_line "Unproccessed tokens in statement" x.line_num
       else AssignmentStatement (x, y, z, expression)
   | x :: xs when is_if_statement x -> parse_if_block [] [] true xs
   | [ x; y ] when is_print_statement x y -> PrintStatement y
-  | _ -> raise (Failure "Invalid statement format\n")
+  | _ -> fatal_err "Unrecognised statement type"
 
 and parse_if_block acc_condition acc_segments has_valid_start = function
   | x :: xs -> (
@@ -99,19 +91,17 @@ and parse_if_block acc_condition acc_segments has_valid_start = function
               parse_expression None [] (List.rev acc_condition)
             in
             if List.length expr_remaining > 0 then
-              raise (Failure "Invalid if statement condition")
+              fatal_err_with_line "Invalid if statement condition" x.line_num
             else
               parse_if_block []
                 ((condition, block) :: acc_segments)
                 false remaining
-          else raise (Failure "Invalid if statement condition location")
+          else unexpected_token x
       | T_ELIF ->
-          if has_valid_start then
-            raise (Failure "Invalid location for elif statement")
+          if has_valid_start then unexpected_token x
           else parse_if_block [] acc_segments true xs
       | T_ELSE ->
-          if has_valid_start then
-            raise (Failure "Invalid location for else statement")
+          if has_valid_start then unexpected_token x
           else parse_if_block [ x ] acc_segments true xs
       | _ -> parse_if_block (x :: acc_condition) acc_segments has_valid_start xs
       )
@@ -139,7 +129,7 @@ and parse_statements acc_statements current_statement nested_level = function
             xs)
   | [] ->
       if List.length current_statement > 0 then
-        raise (Failure "Invalid end of statements")
+        fatal_err "Unexpected end of statements"
       else List.rev acc_statements
 
 and parse_block nested_level current_block = function
@@ -151,12 +141,12 @@ and parse_block nested_level current_block = function
           else parse_block (nested_level - 1) (x :: current_block) xs
       | T_OPEN_BLOCK -> parse_block (nested_level + 1) (x :: current_block) xs
       | _ -> parse_block nested_level (x :: current_block) xs)
-  | [] -> raise (Failure "Unclosed block")
+  | [] -> fatal_err "Unclosed block"
 
 let create_tree = function
   | { t_type = T_OPEN_BLOCK; _ } :: xs ->
       let statements, remaining = parse_block 0 [] xs in
       if List.length remaining > 0 then
-        raise (Failure "Unprocessed tokens at end of program")
+        fatal_err "Unprocessed tokens at end of program"
       else Program statements
-  | _ -> raise (Failure "All programs must be inside a block")
+  | _ -> fatal_err "All programs must be inside a block"

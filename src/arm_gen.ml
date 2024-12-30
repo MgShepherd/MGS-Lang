58c0772ee86bb2ae7974e0daa296559193eb7f1d
MgShepherd <michaelshep1504@gmail.com>
2024-12-30 11:50:38 +0000

    Made arm_gen use logger file for error handling

--- src/arm_gen.ml
+++ src/arm_gen.ml
@@ -1,4 +1,5 @@
 open Parser
+open Logger
 module StringMap = Map.Make (String)
 
 let sys_exit = 93
@@ -23,43 +24,41 @@ let process_comparison_operator = function
   | "<=" -> "LE"
   | _ -> ""
 
-let get_value_from_map map key =
-  try StringMap.find key map
+let get_value_from_map map (var_token : Token.token) =
+  try StringMap.find var_token.t_str map
   with _ ->
-    let error_message = Printf.sprintf "Unable to find key %s in map\n" key in
-    raise (Failure error_message)
+    let msg = Printf.sprintf "Undefined variable: %s" var_token.t_str in
+    fatal_err_with_line msg var_token.line_num
 
-let process_variable current_reg var_name stack_vars =
-  let value = get_value_from_map stack_vars var_name in
+let process_variable current_reg (var_token : Token.token) stack_vars =
+  let value = get_value_from_map stack_vars var_token in
   Printf.sprintf "\tLDR W%d, [X%d, #-%d]\n" current_reg frame_pointer_register
     value
 
 let rec process_expression current_reg expr stack_vars label_num block_num =
-  if current_reg > 30 then raise (Failure "Too many operands in expression")
-  else
-    match expr with
-    | ExprToken x -> (
-        match x.t_type with
-        | T_NUMBER -> Printf.sprintf "\tMOV X%d, #%s\n" current_reg x.t_str
-        | T_VARIABLE -> process_variable current_reg x.t_str stack_vars
-        | T_ELSE -> Printf.sprintf "\tB _%dif%d\n" block_num label_num
-        | _ -> "")
-    | ExprArithmetic (operator, left, right) ->
-        Printf.sprintf "%s%s\t%s X%d, X%d, X%d\n"
-          (process_expression (current_reg + 1) left stack_vars label_num
-             block_num)
-          (process_expression (current_reg + 2) right stack_vars label_num
-             block_num)
-          (process_arithmetic_operator operator.t_str)
-          current_reg (current_reg + 1) (current_reg + 2)
-    | ExprComparison (operator, left, right) ->
-        Printf.sprintf "%s%s\tCMP X%d, X%d\n\tB.%s _%dif%d\n"
-          (process_expression current_reg left stack_vars label_num block_num)
-          (process_expression (current_reg + 1) right stack_vars label_num
-             block_num)
-          current_reg (current_reg + 1)
-          (process_comparison_operator operator.t_str)
-          block_num label_num
+  match expr with
+  | ExprToken x -> (
+      match x.t_type with
+      | T_NUMBER -> Printf.sprintf "\tMOV X%d, #%s\n" current_reg x.t_str
+      | T_VARIABLE -> process_variable current_reg x stack_vars
+      | T_ELSE -> Printf.sprintf "\tB _%dif%d\n" block_num label_num
+      | _ -> "")
+  | ExprArithmetic (operator, left, right) ->
+      Printf.sprintf "%s%s\t%s X%d, X%d, X%d\n"
+        (process_expression (current_reg + 1) left stack_vars label_num
+           block_num)
+        (process_expression (current_reg + 2) right stack_vars label_num
+           block_num)
+        (process_arithmetic_operator operator.t_str)
+        current_reg (current_reg + 1) (current_reg + 2)
+  | ExprComparison (operator, left, right) ->
+      Printf.sprintf "%s%s\tCMP X%d, X%d\n\tB.%s _%dif%d\n"
+        (process_expression current_reg left stack_vars label_num block_num)
+        (process_expression (current_reg + 1) right stack_vars label_num
+           block_num)
+        current_reg (current_reg + 1)
+        (process_comparison_operator operator.t_str)
+        block_num label_num
 
 (*** For now can only assign 16 bit values***)
 let process_assignment stack_vars var_name label_num expr =

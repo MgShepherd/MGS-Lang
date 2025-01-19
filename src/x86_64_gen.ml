eb498f8f08825a468b228452b7f969341d4555bf
MgShepherd <michaelshep1504@gmail.com>
2025-01-19 18:25:38 +0000

    Setup basic assembly generation for x86_64

--- src/x86_64_gen.ml
+++ src/x86_64_gen.ml
@@ -1,2 +1,6 @@
-let generate_assembly _program =
-  "This is a placeholder for x86_64 assembly generation"
+let create_start_function = ".global _start\n_start:\n"
+
+let create_exit_function status =
+  Printf.sprintf "\tMOV $60, %%rax\n\tMOV $%d, %%rdi\n\tsyscall\n" status
+
+let generate_assembly _program = create_start_function ^ create_exit_function 0
